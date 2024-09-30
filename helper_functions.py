9e2858f685f17d618c09c785fb194eac244e03b6
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2024-09-30 22:18:19 +0100

    Add documentation

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,16 @@
 def make_category_list(index_dict):
+    """Makes a list of categories from the index to plot graphs
+
+    Parameters
+    ----------
+    index_dict : dict
+        Index dictionary read from index.csv
+
+    Returns
+    -------
+    category_list : list
+        List of categories to use in plots
+    """
     category_list = []
     for item in index_dict.values():
         category = item['Category']
@@ -7,18 +19,47 @@ def make_category_list(index_dict):
     return(category_list)
 
 def make_long_text(item, show_category=False):
-    if show_category:
-        category = item['Category']
+    """Makes a long text to use in plots
+
+    Parameters
+    ----------
+    item : dict
+        an item from the index dictionary read from
+        index.csv
+
+    Returns
+    -------
+    text : string
+        Concatentation of certain fields in the index dictorionary
+    """
+
     title = item['Short_Title']
     author = item['Short_Author']
     year = item['Year']
     if show_category:
+        #used for dendogram
+        category = item['Category']
         text = f'{category}-{title}-{author}({year})'
     else:
+        #shorter version intended for scatter plot
         text = f'{title}-{author}({year})'
     return(text)
 
 def make_book_name_list(index_dict):
+    """Makes a list of book names to use in plots
+    by looping round the index dictionary read from index.csv
+
+    Parameters
+    ----------
+    index_dict : dict
+        Index dictionary read from index.csv
+
+    Returns
+    -------
+    book_names_list : list
+        List of book names to use in plots
+    """
+    
     book_names_list = []
     for item in index_dict.values():
         text = make_long_text(item, show_category=True)
