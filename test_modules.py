c77cbd8dd99ccfda958bfad1321e6e6e3501a39c
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2024-10-03 22:04:18 +0100

    Testing

---
+++ test_modules.py
@@ -0,0 +1,21 @@
+from text_functions import read_text, read_index
+from text_functions import read_text_files
+
+ENCODING = 'utf-8-sig'
+PATH = 'test_data/'
+
+def test_read_text():
+    """Check distance read for an array element"""
+    filename = PATH + 'test_data1.txt'
+    text = read_text(filename, ENCODING)
+    expected_text = ' test data more data'
+    assert expected_text == text
+
+def test_full_process():
+    """Test end process from index read to corpus production"""
+    filename = PATH + 'test_index.csv'
+    index = read_index(filename, ENCODING)
+    corpus = read_text_files(index, ENCODING, PATH)
+    expected_corpus = [' test data more data', ' test data more data words']
+    assert expected_corpus == corpus
+
